64c0cc2744a0a995e3e23f512ff6f75b337a8599
Daniel <danielsilva.ita@gmail.com>
2014-02-16 01:24:47 -0500

    MessageGenerator class

---
+++ src/MessageGenerator.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class MessageGenerator {
+
+	public static void main(String[] args) {
+		
+		try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			String line = br.readLine();
+			String[] lineSplit;
+			
+			while (line != null) {
+				lineSplit = line.split("\\s+",3);
+				bw.append(lineSplit[0]+","+lineSplit[0]+" "+lineSplit[1]+"\n");
+				bw.append(lineSplit[1]+","+lineSplit[0]+" "+lineSplit[1]+"\n");
+				line = br.readLine();
+			}
+			
+			
+			bw.flush();
+			br.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
